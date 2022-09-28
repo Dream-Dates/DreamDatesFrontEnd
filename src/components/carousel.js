5fe8f066555acff42d2d52e57cf583792f2b55c0
Alvin Chou <alvinchou516@hotmail.com>
2022-09-27 20:18:57 -0400

    feat: :lipstick: disable the prev / next button at start or end of carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,34 +1,65 @@
 // Carousel.js
+import { useState } from "react";
 import leftArrow from "../assets/leftArrow.svg";
 import rightArrow from "../assets/rightArrow.svg";
 
 function Carousel({ data }) {
+    const [leftDisable, setLeftDisabled] = useState(true)
+    const [rightDisable, setRightDisabled] = useState(false)
+
     const slideLeft = () => {
         const slider = document.getElementById("slider");
         slider.scrollLeft = slider.scrollLeft - 300;
-    };
 
+        setTimeout(() => {
+            checkSlideLocation(slider.scrollLeft, slider.scrollWidth)
+        }, 300);
+    };
+    
     const slideRight = () => {
         const slider = document.getElementById("slider");
         slider.scrollLeft = slider.scrollLeft + 300;
+
+        setTimeout(() => {
+            checkSlideLocation(slider.scrollLeft, slider.scrollWidth)
+        }, 300);
     };
+
+    // check the value of scrollLeft is less than 290 disable left button, if scrollwidth - left - 300 is less than 300 disable right button
+    const checkSlideLocation = (left, width) => {
+            if (left < 290) {
+                console.log('left disabled')
+                setLeftDisabled(true)
+            } else {
+                console.log('left is enabled')
+                setLeftDisabled(false)
+            }
+
+            if (width - 300 - left < 300 ) {
+                console.log('right disabled')
+                setRightDisabled(true)
+            } else {
+                console.log('right is enabled')
+                setRightDisabled(false)
+            }
+    }
 
     return (
         <div className="carousel">
-            <button className="carouselControls prev" onClick={slideLeft}>
+            <button className={`carouselControls prev ${leftDisable && 'disable-link'}`} onClick={slideLeft}>
                 {" "}
                 <img src={leftArrow} alt="previous" />{" "}
             </button>
             <div className="carouselContainer" id="slider">
-                {data.map((item) => {
+                {data.map((item, index) => {
                     return (
-                        <div className="carouselCard">
+                        <div className="carouselCard" key={index}>
                             <img src={item} alt=".dd" />
                         </div>
                     );
                 })}
             </div>
-            <button className="carouselControls next" onClick={slideRight}>
+            <button className={`carouselControls next ${rightDisable && 'disable-link'}`} onClick={slideRight}>
                 {" "}
                 <img src={rightArrow} alt="next" />{" "}
             </button>
