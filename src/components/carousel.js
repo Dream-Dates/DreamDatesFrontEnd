57ab7a75c04abaf3ca444fc3864964410daa3377
Alvin Chou <alvinchou516@hotmail.com>
2022-12-12 18:03:46 -0500

    refactor: :construction: working on refactoringthe carousel for modal

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,81 +1,72 @@
 // Carousel.js
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import leftArrow from "../assets/leftArrow.svg";
 import rightArrow from "../assets/rightArrow.svg";
 
 function Carousel({ data }) {
     const [leftDisable, setLeftDisabled] = useState(true)
     const [rightDisable, setRightDisabled] = useState(false)
+        const [currentIndex, setCurrentIndex] = useState(0);
+        // const [length, setLength] = useState(0);
 
-    const widthOfScroll = 300 // horizontal scroll
-    let ready = true;
+    const widthOfScroll = 300 // how much horizontal scroll we want
+    let totalContentWidth
 
-    const slideLeft = () => {
-        if (ready) {
-            ready = false
-            const slider = document.getElementById("slider");
-            slider.scrollLeft = slider.scrollLeft - widthOfScroll;
-            
-            setTimeout(() => {
-                checkSlideLocation(slider.scrollLeft, slider.scrollWidth)
-                ready = true
-            }, 300);
+    const next = () => {
+        // get slider element
+        const slider = document.getElementById("slider");
+        // get the total width
+        totalContentWidth = slider.scrollWidth
+
+        if (currentIndex * widthOfScroll < totalContentWidth - widthOfScroll) {
+            setCurrentIndex((prevState) => prevState + 1);
         }
     };
 
-    const slideRight = () => {
-        if (ready) {
-            ready = false
-            const slider = document.getElementById("slider");
-            slider.scrollLeft = slider.scrollLeft + widthOfScroll;
-            
-            setTimeout(() => {
-                checkSlideLocation(slider.scrollLeft, slider.scrollWidth)
-                ready = true
-            }, 300);
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex((prevState) => prevState - 1);
         }
     };
-
-    // check the value of scrollLeft is less than 290 disable left button, if scrollwidth - left - 300 is less than 300 disable right button
-    const checkSlideLocation = (left, width) => {
-        console.log(left)
-            if (left == 0) {
-                console.log('left disabled')
-                setLeftDisabled(true)
-            } else {
-                console.log('left is enabled')
-                setLeftDisabled(false)
-            }
-
-            if (width - widthOfScroll - left < widthOfScroll ) {
-                console.log('right disabled')
-                setRightDisabled(true)
-            } else {
-                console.log('right is enabled')
-                setRightDisabled(false)
-            }
-    }
 
     return (
-        <div className="carousel">
-            <button className={`carouselControls prev ${leftDisable && 'disable-link'}`} onClick={slideLeft}>
-                {" "}
-                <img src={leftArrow} alt="previous" />{" "}
-            </button>
-            <div className="carouselContainer" id="slider">
-                {data.map((item, index) => {
-                    return (
-                        <div className="carouselCard" key={index}>
-                            <img src={item} alt=".dd" />
-                        </div>
-                    );
-                })}
+        <>
+            <div className="carouselContainer">
+                <div className="carouselWindow">
+                    <div
+                        className="carouselContent"
+                        id="slider"
+                        style={{
+                            transform: `translateX(-${currentIndex * 300}px)`,
+                        }}
+                    >
+                        {data.map((item, index) => {
+                            return (
+                                <div className="carouselCard" key={index}>
+                                    <img src={item} alt=".dd" />
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <button
+                        className={`leftArrow ${
+                        currentIndex == 0 && "disable-button"
+                        }`}
+                        onClick={prev}
+                    >
+                        <img src={leftArrow} alt="previous" />
+                    </button>
+                    <button
+                        className={`rightArrow ${
+                            currentIndex * widthOfScroll < totalContentWidth - widthOfScroll && "disable-button"
+                        }`}
+                        onClick={next}
+                    >
+                        <img src={rightArrow} alt="next" />
+                    </button>
+                </div>
             </div>
-            <button className={`carouselControls next ${rightDisable && 'disable-link'}`} onClick={slideRight}>
-                {" "}
-                <img src={rightArrow} alt="next" />{" "}
-            </button>
-        </div>
+        </>
     );
 }
 
